76343aeed4a9730cf04f1aeb5f5be66cb8a57984
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-06-09 14:28:23 -0400

    add .env file with api key and add axios to get data from api in app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+const [weather, setWeather] = useState()
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${process.env.REACT_APP_LAT}&lon=${process.env.REACT_APP_LONG}&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`
+   
+    const getData = async () =>{
+      const response = await axios.get(url);
+      console.log(response.data);
+      // setWeather(prev => {...prev, })
+    }
+    getData()
+      }, [])
+  //  console.log(weather);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
