ad50c13849471755e80cef605204b88610fb7494
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-06-10 11:52:30 -0400

    add moment npm and move api request into a hook file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
+import useData from './useData';
 
 
 function App() {
-  const [weather, setWeather] = useState({});
-  useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${process.env.REACT_APP_LAT}&lon=${process.env.REACT_APP_LONG}&units=metric&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`;
+ const {weather} = useData()
+  // console.log(weather.data);
 
-    const getData = async () => {
-      const response = await axios.get(url);
-      console.log(response.data);
-      
-      setWeather(prev => {
-        return {...prev, data: response.data}})
-    };
-    getData();
-  }, []);
-   console.log('weather', weather);
+    const dailyData = weather.data.daily.map(info => {
+      const icon = `http://openweathermap.org/img/wn/${info.weather[0].icon}.png`
+      return <div>{info.dt}</div> 
+      // info.dt
+    })
+    console.log(dailyData);
   return (
     <div className="App">
-
+      {dailyData}
     </div>
   );
 }
@@ -28,19 +22,23 @@ function App() {
 export default App;
 
 
+
 // let unix_timestamp = response.data.current.dt;
-//       // Create a new JavaScript Date object based on the timestamp
-//       // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-//       var date = new Date(unix_timestamp * 1000);
-//       // Hours part from the timestamp
-//       var hours = date.getHours();
-//       // Minutes part from the timestamp
-//       var minutes = "0" + date.getMinutes();
-//       // Seconds part from the timestamp
-//       var seconds = "0" + date.getSeconds();
-//       console.log(date);
+// // Create a new JavaScript Date object based on the timestamp
+// // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+// var date = new Date(unix_timestamp * 1000);
+// // Hours part from the timestamp
+// var hours = date.getHours();
+// // Minutes part from the timestamp
+// var minutes = "0" + date.getMinutes();
+// // Seconds part from the timestamp
+// var seconds = "0" + date.getSeconds();
+// console.log('date',date);
+// console.log('getdate',date.getDate());
+// console.log('getday',date.getDay());
+// console.log('getMonth',date.getMonth());
 
-//       // Will display time in 10:30:23 format
-//       var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+// // Will display time in 10:30:23 format
+// var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
-//       console.log(formattedTime);
+// console.log(formattedTime);
---
+++ src/useData.js
@@ -0,0 +1,24 @@
+import { React, useEffect, useState } from 'react';
+import axios from 'axios'
+
+
+const useData = () => {
+
+  const [weather, setWeather] = useState({});
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${process.env.REACT_APP_LAT}&lon=${process.env.REACT_APP_LONG}&units=metric&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`;
+    
+    const getData = async () => {
+      const response = await axios.get(url);
+      
+      setWeather(prev => {
+        return {...prev, data: response.data}})
+        
+      
+      };
+      getData();
+    }, []);
+
+    return { weather }
+  }
+  export default useData
